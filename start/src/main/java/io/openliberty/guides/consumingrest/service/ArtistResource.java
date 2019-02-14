d1d9c5e011fe9def7e21dd1aa7acf0e415a91c38
Gilbert Kwan <gkwan@ca.ibm.com>
2019-02-14 15:27:29 -0500

    restore ArtistResource.java

---
+++ start/src/main/java/io/openliberty/guides/consumingrest/service/ArtistResource.java
@@ -0,0 +1,43 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ // end::comment[]
+package io.openliberty.guides.consumingrest.service;
+
+import javax.json.JsonArray;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+import io.openliberty.guides.consumingrest.model.Artist;
+//import io.openliberty.guides.consumingrest.Consumer;
+
+@Path("artists")
+public class ArtistResource {
+
+    @Context
+    UriInfo uriInfo;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonArray getArtists() {
+    	return Reader.getArtists();
+    }
+    
+    //Add additional methods here
+
+}
