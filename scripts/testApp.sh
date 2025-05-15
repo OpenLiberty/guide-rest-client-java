34ce58d30902b87e6651bb8fc1a0be178c5e45b1
George Zhang <geetransit@gmail.com>
2025-05-15 17:05:42 -0400

    Update test script to use mvnw

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 ##############################################################################
 ##
@@ -7,10 +8,10 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn -ntp liberty:start
-mvn -ntp failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -ntp liberty:start
+./mvnw -ntp failsafe:integration-test liberty:stop
+./mvnw -ntp failsafe:verify
